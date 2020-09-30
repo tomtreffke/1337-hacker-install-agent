8f748e73aab12410dc9ebcd4b4d0a2e98a14b707
Tom Treffke <tom.treffke@outlook.com>
2020-09-30 12:41:16 +0200

    initial commit

---
+++ arch-post-install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+if [[ $EUID -ne 0 ]]; then
+   	echo "This script must be run as root" 
+   	exit 1
+else
+    echo "exec pacman -Yu"
+    pacman -Syu
+    
+    echo "exec pacman -Yu"
+    apt-get upgrade -y
+    
+    clear
+    echo "starting software installation - basics"
+    apt-get install wget curl git teamviewer
+    
+    echo "starting software installation - web"
+    apt-get install google-chrome-stable chromium-browser firefox
+    
+    echo "starting software installation - dev"
+    apt-get install vscode
+    
+    echo "installing most important piece of software"
+    apt-get install cmatrix
+
+
+
+
+
+
+
+    # end of script
+    cmatrix   
+fi
