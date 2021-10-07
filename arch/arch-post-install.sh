cb6d0dabb0af68c82562e31670fdfe53e0fedd4c
t.treffke <t.treffke@go2cloud-hpc.de>
2021-10-07 08:34:25 +0200

    tested installation with vagrant, 80%, some work left

--- arch/arch-post-install.sh
+++ arch/arch-post-install.sh
@@ -4,31 +4,48 @@ if [[ $EUID -ne 0 ]]; then
    	exit 1
 else
     echo "exec pacman -Yu"
-    pacman -Syu
+    pacman -Syu --noconfirm
     
     clear
     echo "starting software installation - basics"
-    pacman -Syu wget curl git teamviewer zsh oh-my-zsh zsh-theme-powerlevel10k libreoffice dislocker gnome-shell-extensions
+    pacman -Syu --noconfirm wget curl git teamviewer zsh oh-my-zsh zsh-theme-powerlevel10k libreoffice dislocker gnome-shell-extensions os-prober openssh npm man-pages yay
     
     echo "starting software installation - web"
-    pacman -Syu chromium firefox 
+    pacman -Syu --noconfirm chromium firefox 
     
+    # no vscode, I'll install this from binary (deb) and convert it to pacman-managable package.
     echo "starting software installation - dev"
-    pacman -Syu vscode podman buildah ansible filezilla htop vagrant go go-tools 
+    pacman -Syu --noconfirm \
+        podman \
+        buildah \
+        ansible \
+        filezilla \
+        htop \
+        vagrant \
+        packer \
+        go \
+        go-tools \
+        go-yq-bin \
+        azure-cli \
+        kubectl \
+        helm \
+
 
     echo "\n\n +++++++++++++++++++++ \n 
     starting virtualization setup"
-    pacman -Syu libvirt qemu virt-manager
+    pacman -Syu --noconfirm libvirt qemu virt-manager
 
     echo "connectivity"
-    pacman -Syu forticlient-vpn
 
     echo "starting software installation - entertainment"
 
-    pacman -Syu spotify
+    pacman -Syu --noconfirm spotify
     
     echo "installing most important piece of software"
-    pacman -Syu install cmatrix
+    pacman -Syu --noconfirm cmatrix
+
+    echo "AUR packages"
+    pacman -Syu --noconfirm teams
 
 
     echo "loading ZSH config"
